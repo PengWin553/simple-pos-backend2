d415527aff08f57c92cbd9599b72662e961af23e
PengWin553 <158315168+PengWin553@users.noreply.github.com>
2024-08-14 23:30:32 +0800

    Revise SaveTransactionProduct to  SaveTransactionProductsBatch

--- Controllers/TransactionProductApiController.cs
+++ Controllers/TransactionProductApiController.cs
@@ -24,13 +24,29 @@ namespace simple_pos_backend2.Controllers
         }
 
         // create transaction product
-        [HttpPost("SaveTransactionProduct")]
-        public async Task<IActionResult> SaveTransactionProductAsync(TransactionProduct transactionProduct)
-        {
-            const string query = "Insert into TransactionProducts (TransactionId, ProductId, Quantity) Values (@TransactionId, @ProductId, @Quantity); Select tp.TransactionProductId, tp.TransactionId, tp.ProductId, p.ProductName, p.Price, tp.Quantity from TransactionProducts tp Inner Join Products p on tp.ProductId = p.ProductId Where tp.TransactionId = @TransactionId";
+        // [HttpPost("SaveTransactionProduct")]
+        // public async Task<IActionResult> SaveTransactionProductAsync(TransactionProduct transactionProduct)
+        // {
+        //     const string query = "Insert into TransactionProducts (TransactionId, ProductId, Quantity) Values (@TransactionId, @ProductId, @Quantity); Select tp.TransactionProductId, tp.TransactionId, tp.ProductId, p.ProductName, p.Price, tp.Quantity from TransactionProducts tp Inner Join Products p on tp.ProductId = p.ProductId Where tp.TransactionId = @TransactionId";
 
-            var result = await _connection.QueryAsync<TransactionProduct>(query, transactionProduct);
-            return Ok(result);
+        //     var result = await _connection.QueryAsync<TransactionProduct>(query, transactionProduct);
+        //     return Ok(result);
+        // }
+
+        [HttpPost("SaveTransactionProductsBatch")]
+        public async Task<IActionResult> SaveTransactionProductsBatchAsync(List<TransactionProduct> transactionProducts)
+        {
+            const string query = "INSERT INTO TransactionProducts (TransactionId, ProductId, Quantity) VALUES (@TransactionId, @ProductId, @Quantity)";
+            var affectedRows = await _connection.ExecuteAsync(query, transactionProducts);
+
+            if (affectedRows > 0)
+            {
+                return Ok("Transaction products saved successfully.");
+            }
+            else
+            {
+                return BadRequest("Failed to save transaction products.");
+            }
         }
     }
 }
